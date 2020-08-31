cc41f6c33c7a80bdee460841475e4c601193ffa7
wc <wchamberlain89@gmail.com>
2020-08-31 13:53:49 -0700

    Adds 1.8 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 const Statistic = ({ title, value }) => <h5>{title} {value}</h5>
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
+const Statistics = ({ good, neutral, bad }) => {
   const findTotal = () => {
     return good + neutral + bad;
   }
@@ -18,6 +14,25 @@ const App = () => {
   const findPositive = () => {
     return good / findTotal();
   }
+  return (
+    <>
+      <Statistic title={"Good"} value={good} />
+      <Statistic title={"Neutral"} value={neutral} />
+      <Statistic title={"Bad"} value={bad} />
+      <Statistic title={"Total Votes"} value={findTotal()} />
+      <Statistic title={"Average"} value={findAverage() || 0} />
+      <Statistic title={"Positive"} value={findPositive() || 0} />
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
 
   return (
     <>
@@ -26,12 +41,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <Statistic title={"Good"} value={good} />
-      <Statistic title={"Neutral"} value={neutral} />
-      <Statistic title={"Bad"} value={bad} />
-      <Statistic title={"Total Votes"} value={findTotal()} />
-      <Statistic title={"Average"} value={findAverage() || 0} />
-      <Statistic title={"Positive"} value={findPositive() || 0} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
     </>
   )
