98672ecc6604a3488f239c46428eff0879fb594d
wc <wchamberlain89@gmail.com>
2020-09-01 08:19:43 -0700

    Add solution for 2.7 and 2.8

--- part2/the_phonebook/src/index.js
+++ part2/the_phonebook/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
-const CreateForm = ({ onSubmit, onChange }) => {
+const CreateForm = ({ onSubmit, onChange, onUpdateNumber }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmit();
@@ -9,7 +9,14 @@ const CreateForm = ({ onSubmit, onChange }) => {
   return (
     <form onSubmit={e => handleSubmit(e)}>
         <div>
-          name: <input onChange={onChange}/>
+          <label>
+            name: 
+          </label>
+          <input name="name" onChange={onChange} />
+          <label>
+            number: 
+          </label>
+          <input name="phoneNumber" onChange={onChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -21,30 +28,54 @@ const CreateForm = ({ onSubmit, onChange }) => {
 const PhoneNumbers = ({ entries }) => {
   return (
     <>
-      { entries.map((entry, index) => <h5 key={index}>{entry.name}</h5>) }
+      { entries.map((entry, index) => {
+        return (
+          <>
+          <h5 key={index}>{entry.name}</h5>
+          <h5 key={index}>{entry.phoneNumber}</h5>
+          </>
+        ) 
+      })}
     </>
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: "971-444-4040" }
   ])
-  const [ newName, setNewName ] = useState('')
+  const [ newPerson, setNewPerson ] = useState({
+  });
   
   const handleSubmit = () => {
-    setPersons(persons.concat({name: newName}))
-    console.log(persons)
+    const isValid = validateName(newPerson);
+    isValid && setPersons(persons.concat({ name: newPerson.name, phoneNumber: newPerson.phoneNumber }))
+  }
+  
+  const onChange = (event) => {
+    setNewPerson({ ...newPerson, [event.target.name]: event.target.value });
+    console.log(newPerson);
   }
 
-  const updateNewName = (event) => {
-    setNewName(event.target.value);
-  }
+  const validateName = (name) => {
+    const validation = persons.filter( person => {
+      return (
+        person.name === name
+        ) 
+      })
+
+      if(validation.length > 0) {
+        alert(`${name} is already added to the phonebook`);
+      }  
+        
+      return validation.length < 1;
+   }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <CreateForm onSubmit={handleSubmit} onChange={updateNewName}/>
+      <CreateForm onSubmit={handleSubmit} onChange={onChange} />
       <h2>Numbers</h2>
       <PhoneNumbers entries={persons} />
     </div>
