309e9cbc2b1e72da864e8a76f29d487f84ccd456
wc <wchamberlain89@gmail.com>
2020-09-01 20:47:05 -0700

    Adds solution for 3.4

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-const persons = [{id: 0, name: "ward chamberlain", number: "971-404-7927"}, {id: 1, name: "Kayla Crumb", number: "971-404-8511"}]
+let persons = [{id: 0, name: "ward chamberlain", number: "971-404-7927"}, {id: 1, name: "Kayla Crumb", number: "971-404-8511"}]
 
 app.get("/api/persons", (req, res) => {
   res.json(persons)
@@ -17,6 +17,13 @@ app.get("/api/persons/:id", (req, res) => {
 app.get("/info", (req, res) => {
   const date = new Date();
   res.send(`<p>Phonebook has information for ${persons.length} people.</p><p>${date}</p>`);
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  console.log(`Deleted person with id ${id}`)
+  response.status(204).end()
 })
 
 app.listen(PORT, () => {
