b3d410d75201556947eb61c2d6deef6c8ea705df
wc <wchamberlain89@gmail.com>
2020-09-01 20:31:21 -0700

    Adds solution for 3.3

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-const persons = [{name: "ward chamberlain", number: "971-404-7927"}, {name: "Kayla Crumb", number: "971-404-8511"}]
+const persons = [{id: 0, name: "ward chamberlain", number: "971-404-7927"}, {id: 1, name: "Kayla Crumb", number: "971-404-8511"}]
 
 app.get("/api/persons", (req, res) => {
   res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  res.json(person)
 })
 
 app.get("/info", (req, res) => {
   const date = new Date();
   res.send(`<p>Phonebook has information for ${persons.length} people.</p><p>${date}</p>`);
 })
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 });
