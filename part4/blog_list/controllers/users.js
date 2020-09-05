1b103832d973b3b9d845f33181912122ae1445b0
wc <wchamberlain89@gmail.com>
2020-09-04 22:03:56 -0700

    adds password length validation

--- part4/blog_list/controllers/users.js
+++ part4/blog_list/controllers/users.js
@@ -2,8 +2,24 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/users')
 
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User.find({})
+    if(users) {
+      response.status(200).json(users)
+    }
+    response.status(400).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
 usersRouter.post('/', async (request, response) => {
   const body = request.body
+
+  if(body.password.length < 3) {
+    return response.status(400).json({ error: 'password must be more than 3 characters' })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
