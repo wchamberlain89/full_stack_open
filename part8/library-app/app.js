6494193af2072a0459fa1b3a239c6e2a3a16c77c
wc <wchamberlain89@gmail.com>
2020-09-23 17:25:27 -0700

    adds solution 8.13

--- part8/library-app/app.js
+++ part8/library-app/app.js
@@ -1,88 +1,8 @@
+require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server')
 const { v1: uuid } = require('uuid')
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const db = require('./db')
+const Book = require('./models/book')
 
 const typeDefs = gql`
   type Book {
@@ -98,7 +18,7 @@ const typeDefs = gql`
     born: Int
     bookCount: Int!
   }
-  input editAuthorParams {
+  input EditAuthorParams {
     name: String
     born: Int
   }
@@ -110,7 +30,7 @@ const typeDefs = gql`
   }
   type Mutation {
     addBook(title: String!, author: String!, published: Int!, genres: [String!]): Book
-    editAuthor(name: String!, params: editAuthorParams): Author
+    editAuthor(name: String!, params: EditAuthorParams): Author
   }
 `
 
@@ -120,6 +40,8 @@ const resolvers = {
       const { author } = args
       const { genre } = args
       console.log('getting books')
+      const books = Book.find({})
+      console.log('books are ', books)
       if(author) {
         return books.filter(book => book.author === author)
       }
@@ -137,10 +59,9 @@ const resolvers = {
   Mutation: {
     addBook: (root, args) => {
       console.log('Adding book')
-      const book = { ...args, id: uuid() }
+      const book = new Book({ ...args })
       console.log('Adding book', book)
-      
-      books = books.concat(book)
+      book.save()
       
       const isNewAuthor = !authors.find(author => author.name === book.author)
 
